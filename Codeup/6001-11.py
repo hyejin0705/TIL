6d7e8b1d4702d118ffc57fbfa48227fc7748c1bb
hyejin0705 <hyejin3681@gmail.com>
2022-07-19 17:37:51 +0900

    0719

---
+++ Codeup/6001-11.py
@@ -0,0 +1,32 @@
+# 6001 출력하기01
+print("Hello")
+
+# 6002 출력하기02
+print("Hello", "World")
+
+# 6003 출력하기03
+print("Hello", "World", sep = '\n')
+
+# 6004 출력하기04
+print("'Hello'")
+
+# 6005 출력하기05
+print('"Hello World"')
+
+# 6006 출력하기06
+print('\"!@#$%^&*()\'')
+
+# 6007 출력하기07
+print('''"C:\\Download\\'hello'.py"''')
+
+# 6008 출력하기08
+print('''print("Hello\\nWorld")''')
+
+# 6009 문자 1개 입력받아 그대로 출력하기
+print(input())
+
+# 6010 정수 1개 입력받아 int로 변환하여 출력하기
+print(int(input()))
+
+# 6011 실수 1개 입력받아 변환하여 출력하기
+print(float(input()))
