23e3116a2404dd7acc349316819a0751915652c3
hyejin0705 <hyejin3681@gmail.com>
2022-10-20 17:57:36 +0900

    1020

--- 프로젝트/6주차/mypjt/accounts/views.py
+++ 프로젝트/6주차/mypjt/accounts/views.py
@@ -1,13 +1,15 @@
-from multiprocessing import AuthenticationError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
 # Create your views here.
+@require_http_methods(['GET', 'POST'])
 def login(request):
     if request.user.is_authenticated:
         return redirect('articles:index')
@@ -25,15 +27,17 @@ def login(request):
         'form': form
     }
     
-    return render(request, 'accouts/login.html', context)
+    return render(request, 'accounts/login.html', context)
 
 
+@require_POST
 def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect('articles:index')
 
 
+@require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.user.is_authenticated:
         return redirect('article:index')
@@ -55,6 +59,7 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
+@require_POST
 def delete(request):
     if request.user.is_authenticated:
         request.user.delete()
@@ -62,6 +67,8 @@ def delete(request):
     return redirect('articles:index')
 
 
+@login_required
+@require_http_methods(['GET', 'POST'])
 def update(request):
     if request.method == "POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
@@ -79,6 +86,8 @@ def update(request):
     return render(request, 'accounts/update.html', context)
 
 
+@login_required
+@require_http_methods(['GET', 'POST'])
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
