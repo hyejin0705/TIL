935cfe6e348493b5190375b36d981820af62a3cb
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-10-23 23:59:34 +0900

    [Gold V] Title: 경쟁적 전염, Time: 636 ms, Memory: 31860 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,35 +1,42 @@
-from collections import deque
- 
+## 문제를 풀 때 생각했던 점
+
+# bfs로 푸는데 초를 추가하는 방법  (어느 시점에 count 해야 할지)
+# 낮은 바이러스부터 시작하는 방법  (정렬)
+
+def bfs(S):
+    q = []
+
+    for i in range(N):
+        for n in range(1, K + 1):
+            if n in arr[i]:
+                j = arr[i].index(n)
+                q.append([arr[i][j], i, j])     # 번호도 같이 저장해줘야 함.
+
+    q.sort()              # 정렬하지 않으면, 낮은 숫자부터 해결하지 못함.
+
+    cnt = 0
+    while q:
+        if cnt == S:
+            break
+
+        for _ in range(len(q)):           # 초를 세기 위해 for문 하나도 생성.
+            virus, si, sj = q.pop(0)
+
+            for di, dj in [[1, 0], [0, 1], [-1, 0], [0, -1]]:         # 4방향 탐색
+                ni, nj = si + di, sj + dj
+                if 0 <= ni < N and 0 <= nj < N and not arr[ni][nj]:   # 범위 내이고, 0이라면,
+                    arr[ni][nj] = virus                               # 바이러스 저장
+                    q.append([virus, ni, nj])                      # 이동할 지역으로 저장.
+
+        cnt += 1          # 한번의 반복이 끝나면, 1초 증가
+
+
 N, K = map(int, input().split())
-graph = []
-virus = []
-for i in range(N):
-  graph.append(list(map(int, input().split())))
-  for j in range(N):
-    if graph[i][j] != 0:
-      virus.append(((graph[i][j], i, j)))
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
 S, X, Y = map(int, input().split())
- 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
- 
-def bfs(s, X, Y):
-  q = deque(virus)
-  count = 0
-  while q:
-    if count == s:
-      break
-    for _ in range(len(q)):
-      prev, x, y = q.popleft()
-      for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < N:
-          if graph[nx][ny] == 0:
-            graph[nx][ny] = graph[x][y]
-            q.append((graph[nx][ny], nx, ny))
-    count += 1
-  return graph[X-1][Y-1]
- 
-virus.sort()
-print(bfs(S, X, Y))
+
+bfs(S)
+
+print(arr[X-1][Y-1])       # 확인할 지역 출력 (인덱스는 0부터 시작이기 때문에 -1)
