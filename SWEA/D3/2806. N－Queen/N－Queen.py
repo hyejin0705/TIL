14134c4709f9c0974562fd70cccde9fd34834293
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-10-26 00:18:32 +0900

    [D3] Title: N-Queen, Time: 238 ms, Memory: 63,828 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -0,0 +1,42 @@
+def check(si, sj):
+    for di, dj in [[-1, -1], [-1, 1]]:
+        ni, nj = si + di, sj + dj
+        while 0 <= ni < N and 0 <= nj < N:
+            if chess[ni][nj]:
+                return 0
+            else:
+                ni += di
+                nj += dj
+
+    else:
+        return 1
+
+
+def dfs(n):
+    global ans
+    if n == N:
+        ans += 1
+        return
+
+    for j in range(N):
+        if not v[j] and check(n, j):
+            v[j] = chess[n][j] = 1
+            dfs(n + 1)
+            v[j] = chess[n][j] = 0
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    chess = [[0] * N for _ in range(N)]
+
+    ans = 0
+
+    v = [0] * N
+
+    dfs(0)
+    
+    # print(ans)
+    print(f'#{test_case} {ans}')
