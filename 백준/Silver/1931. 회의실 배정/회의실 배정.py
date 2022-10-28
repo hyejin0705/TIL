d856706a427559638e0f202ef3525096a9b5fd8b
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-10-28 17:37:29 +0900

    [Silver I] Title: 회의실 배정, Time: 6284 ms, Memory: 59808 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,18 @@
+## pypy로 돌리면, 2280 ms 나옴
+
 N = int(input())
 
 lst = [list(map(int, input().split())) for _ in range(N)]
 
 lst.sort(key=lambda x: [x[1], x[0]])
-# 끝나는 시간으로 정렬 -> 같다면, 시작하는 시간으로 정렬
+# 디버깅: 끝나는 시간으로 정렬 -> 같다면, 시작하는 시간으로 정렬
 
 cnt = 0
 while lst:
     cnt += 1
     sh, eh = lst.pop(0)
 
-    while lst and eh > lst[0][0]:
+    while lst and eh > lst[0][0]:    # 앞에 끝나는 시간보다 시작하는 시간이 작다면, 패스
         lst.pop(0)
 
 print(cnt)
