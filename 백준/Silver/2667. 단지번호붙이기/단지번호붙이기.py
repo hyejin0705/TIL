22e4c71c72665425acd927b15f0c3ce3e75d567c
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-10-30 00:56:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,36 @@
+def dfs(i, j):
+    q = [[i, j]]
+    v[i][j] = 1
+    cnt = 1
+
+    while q:
+        si, sj = q.pop(0)
+
+        for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            ni, nj = si + di, sj + dj
+
+            if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] and not v[ni][nj]:
+                q.append([ni, nj])
+                v[ni][nj] = 1
+                cnt += 1
+
+    return cnt
+
+
+N = int(input())
+
+arr = [list(map(int, input())) for _ in range(N)]
+
+v = [[0] * N for _ in range(N)]
+
+ans = []
+
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] and not v[i][j]:
+            ans.append(dfs(i, j))
+
+ans.sort()
+# 디버깅: 각 단지내 집의 수를 오름차순으로 정렬
+
+print(len(ans), *ans, sep='\n')
