344b755ab0bfeb89d8445f490deb8392e9c6315b
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-10-31 23:44:12 +0900

    [Silver II] Title: 최소 힙, Time: 156 ms, Memory: 36764 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,16 +1,45 @@
+## 시간초과 해결하는 게 큰 문제!
+
+## 1. heapq 라이브러리 활용
+## 2. input도 라이브러리 활용 -> sys.stdin.readline()
+
 import sys
-import heapq
+import heapq        # 시간 단축 방법 1
 
 N = int(input())
 
-heap = []
+heap = []           # 힙 리스트 생성
 for _ in range(N):
-    T = int(sys.stdin.readline())
+    T = int(sys.stdin.readline())    # 시간 단축 방법 2
 
     if T == 0:
-        if not heap:
+        if not heap:      # 리스트가 비어 있다면, 0 출력
             print(0)
+            
         else:
-            print(heapq.heappop(heap))
+            print(heapq.heappop(heap))    # 요소가 존재한다면, heapq.heappop(리스트) -> 최소값 출력
+            
     else:
-        heapq.heappush(heap, T)
+        heapq.heappush(heap, T)    # heapq.heappush(리스트, 요소)   ->  리스트에 요소 추가
+         
+        
+        
+## -------------------------------------------------
+## 최소힙을 적용하지 않고 문제를 곧이곧대로 풀이한 코드
+
+# N = int(input())
+
+# lst = []
+# for _ in range(N):
+#     T = int(input())
+
+#     if T == 0:
+#         if not lst:
+#             print(0)
+
+#         else:
+#             lst.sort()
+#             print(lst.pop(0))
+
+#     else:
+#         lst.append(T)
