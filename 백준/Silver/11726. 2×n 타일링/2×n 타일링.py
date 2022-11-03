6ee17eead9690a716ae374acf3da3e0071560944
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-11-03 23:24:21 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,12 @@
+tile = [0, 1, 2]   # 초기값
+
+# 현재 타일 개수 => 앞의 2개를 더한 값
+# 타일을 붙이는 방법 리스트를 생성
+for idx in range(3, 1001):
+    result = tile[idx-1] + tile[idx-2]
+    tile.append(result)
+
+N = int(input())    # 입력받은 값
+
+# 만든 리스트에서 결과 값  %  10007
+print(tile[N] % 10007)
