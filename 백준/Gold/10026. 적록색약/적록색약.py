ba39cf8bbe372a22f8cb24bbe0e5cd414121c4f2
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-11-07 02:11:19 +0900

    [Gold V] Title: 적록색약, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,25 +1,28 @@
 def bfs(arr):
-    v = [[0] * N for _ in range(N)]
+    v = [[0] * N for _ in range(N)]      # 방문표시 리스트
     q = []
-    cnt = 0
+    cnt = 0      
 
-    for i in range(N):
+    for i in range(N):           # (0, 0)부터 순차 탐색
         for j in range(N):
 
-            if not v[i][j]:
-                v[i][j] = 1
-                cnt += 1
-                q.append([i, j])
+            if not v[i][j]:      # 미방문지라면,
+                v[i][j] = 1      # 방문표시
+                cnt += 1          # 새로운 구역 체크
+                q.append([i, j])  # 인큐
 
-                while q:
+                while q:                # 구역 탐색
                     si, sj = q.pop(0)
 
-                    for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                    for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:    # 4방향 탐색
                         ni, nj = si + di, sj + dj
-
-                        if 0 <= ni < N and 0 <= nj < N and arr[si][sj] == arr[ni][nj] and not v[ni][nj]:
-                            v[ni][nj] = 1
-                            q.append([ni, nj])
+                        
+                        # 이동할 구역이 범위 내이고, 비방문지이고, 앞의 색깔과 같다면,
+                        if 0 <= ni < N and 0 <= nj < N and arr[si][sj] == arr[ni][nj] and not v[ni][nj]:  
+                            # 비방문지라는 조건을 체크하지 않으면, 반복문에서 나올 수 없음ㅡㅜ
+                            
+                            v[ni][nj] = 1        # 방문표시
+                            q.append([ni, nj])   # 인큐
 
     return cnt
 
@@ -32,8 +35,8 @@ arr = [list(input()) for _ in range(N)]
 arr2 = [[0] * N for _ in range(N)]
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 'G':
-            arr2[i][j] = 'R'
+        if arr[i][j] == 'G':         # 적록색약st(G == R) arr 재생성
+            arr2[i][j] = 'R'     
 
         else:
             arr2[i][j] = arr[i][j]
