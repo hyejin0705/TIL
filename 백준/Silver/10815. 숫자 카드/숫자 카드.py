21cdccb67081833e62ea5eb2f12dbe35a479a7f3
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-11-13 00:04:40 +0900

    [Silver V] Title: 숫자 카드, Time: 772 ms, Memory: 114752 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,3 +1,5 @@
+## list보다는 dict가 더 빠름.
+
 import sys
 
 N = int(sys.stdin.readline())
@@ -6,10 +8,29 @@ lst = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 lst2 = list(map(int, sys.stdin.readline().split()))
 
-dic = {}
+dic = {}          # 속도는 dictionary!
 for num in lst:
-    dic[num] = 1
+    dic[num] = 1  # 숫자카드 존재하면, 1 저장
 
 for num in lst2:
-    print(dic.get(num, 0), end=' ')
+    print(dic.get(num, 0), end=' ')  # 숫자카드.get(숫자, 없으면 0)
 print()
+
+
+## -----------------------------------------------------------------
+## 시간초과
+
+# import sys
+
+# N = int(sys.stdin.readline())
+# lst = list(map(int, sys.stdin.readline().split()))
+
+# M = int(sys.stdin.readline())
+# lst2 = list(map(int, sys.stdin.readline().split()))
+
+# for num in lst2:
+#     if num in lst:
+#         print(1, end=' ')
+#         continue
+#     print(0, end=' ')
+# print()
