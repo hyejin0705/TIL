939b6d8f3ddce3b9901359bd636130a54506338e
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-11-14 00:03:43 +0900

    [Silver V] Title: 방 번호, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,13 +1,15 @@
-word = input()
-ans = [0] * 10
-for i in range(len(word)):
-    num = int(word[i])
-    if num == 6 or num == 9:
-        if ans[6] <= ans[9]:
-            ans[6] += 1
-        else:
-            ans[9] += 1
-    else:
-        ans[num] += 1
- 
-print(max(ans))
+# 카운트 배열 (0 ~ 9) 생성
+lst = [0 for i in range(10)]
+
+N = list(map(int, input()))   # 방번호 입력받기
+
+for num in N:
+    lst[num] += 1   # 방번호 하나씩 카운팅
+
+# 6은 9를 뒤집어서 이용할 수 있고,
+# 9는 6을 뒤집어서 이용할 수 있음
+total = lst[6] + lst[9]     # 6와 9가 나온 총합
+lst[6] = total // 2        # 6에는 2로 나눈 몫 입력
+lst[9] = total - lst[6]    # 9에는 6의 값을 뺀 나머지 입력
+
+print(max(lst))     # 카운팅의 최대값 출력
