2aa6f6af8199f458d81dcb1f880f8967046e2172
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-02 23:11:52 +0900

    [Silver I] Title: 곱셈, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,27 @@
+# 분할정복(Divide and Conquer) 문제
+# 10 ^ 11 % 12
+# ((10 ^ 5) * (10 ^ 5) * 10) % 12
+# (((10 ^ 2) ^ 2 * 10) ^ 2 * 10) % 12
+
+import sys
+
+def dac(a, b, c):
+    if b == 1:
+        return a % c    # 그냥 나머지 구하기
+
+    else:
+        # 일단, 함수를 돌리고, ^2하기
+        check = dac(a, b//2, c) ** 2
+
+        if b % 2:                # 지수가 홀수이면,
+            return check * a % c   # a 곱하고, 나머지 구하기
+
+        else:                  # 지수가 짝수이면,
+            return check % c    # 바로 나머지 구하기
+
+
+N, M, K = map(int, sys.stdin.readline().split())
+
+ans = dac(N, M, K)
+
+print(ans)
