c9313f50be3d79ae6ee904f5b49e04965de81ee7
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-07 00:02:35 +0900

    [Gold IV] Title: 문자열 폭발, Time: 348 ms, Memory: 48476 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,3 +1,5 @@
+# 시간초과 해결 >>>> stack 구조를 활용해야 한다. (+ 내장함수 del 활용)
+
 import sys
 
 check = list(sys.stdin.readline().strip())
@@ -7,12 +9,30 @@ stack = []
 
 for ch in check:
     stack.append(ch)
-    # print(stack[-length:])   # out of range 에러 XXXX
+    # print(stack[-length:])   # out of range 에러 해결
 
     if stack[-length:] == remov:
-        del stack[-length:]
+        # stack = stack[-length:]      # 이 또한 시간초과
+        del stack[-length:]        # 내장함수 del 활용하여 시간초과 해결!!!
 
 if stack:
     print(''.join(stack))
 else:
     print('FRULA')
+    
+
+# # ----------------------------------------------------
+# # 시간초과
+# # replace를 사용하니 시간초과 생김...ㅡㅜ
+
+# str = sys.stdin.readline().strip()
+
+# check = sys.stdin.readline().strip()
+
+# while check in str:
+#     str = str.replace(check, '')
+
+# if str:
+#     print(str)
+# else:
+#     print('FRULA')
