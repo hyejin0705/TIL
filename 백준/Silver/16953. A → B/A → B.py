c71f35bac7f5d79d7313818a4c7cac3208c4d461
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-08 23:44:39 +0900

    [Silver II] Title: A → B, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,20 +1,20 @@
 A, B = map(int, input().split())
 
 cnt = 1
-while B != A:
-    if B < A:
+while B != A:     # 같아지는 순간 종료
+    if B < A:         # B < A 보다 작으면, 실패 
         cnt = -1
         break
 
-    if B % 10 == 1:
+    if B % 10 == 1:    # 1의 자리 수가 1이면, 1제거
         B //= 10
         cnt += 1
 
-    elif B % 2 == 0:
+    elif B % 2 == 0:   # 짝수이면, 2로 나누기
         B //= 2
         cnt += 1
 
-    else:
+    else:            # 무한루프로 시간초과 해결방안 >> 홀수일 경우, 실패..
         cnt = -1
         break
 
