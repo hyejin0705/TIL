8d3347965bd697381a778aa730d0d0795c9d0100
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-15 00:23:44 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,20 +1,19 @@
-N, M = map(int, input().split())
-L = list(map(int, input().split()))
-
-L.sort()
-visited = [False] * N
-out = []
-
-def solve(depth, N, M):
-    if depth == M:
-        print(' '.join(map(str, out)))
+def dfs(n, lst):
+    if n == M:
+        print(*lst)
         return
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            out.append(L[i])
-            solve(depth+1, N, M)
-            out.pop()
-            visited[i] = False
 
-solve(0, N, M)
+    for i in range(N):
+        if check[i] not in lst:
+            lst.append(check[i])
+            dfs(n + 1, lst)
+            lst.pop()
+
+
+N, M = map(int, input().split())
+
+check = list(map(int, input().split()))
+
+check.sort()
+
+dfs(0, [])
