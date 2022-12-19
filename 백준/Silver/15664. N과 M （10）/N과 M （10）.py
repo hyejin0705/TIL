3a94d441aa1f9e9f5c90295ea5af352490d7ea65
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-20 00:01:49 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,22 +1,22 @@
-N, M = map(int, input().split())
-L = list(map(int, input().split()))
-
-L.sort()
-visited = [False] * N
-out = []
-
-def solve(depth, idx, N, M):
-    if depth == M:
-        print(' '.join(map(str, out)))
+def dfs(n, ans):
+    if n == N:
+        if len(ans) == M:
+            check.add(tuple(ans))
         return
-    overlap = 0
-    for i in range(idx, N):
-        if not visited[i] and overlap != L[i]:
-            out.append(L[i])
-            visited[i] = True
-            overlap = L[i]
-            solve(depth+1, i+1, N, M)
-            out.pop()
-            visited[i] = False
 
-solve(0, 0, N, M)
+    dfs(n + 1, ans + [lst[n]])
+    dfs(n + 1, ans)
+
+
+N, M = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+lst.sort()
+
+check = set()
+
+dfs(0, [])
+
+for i in sorted(list(check)):
+    print(*i)
