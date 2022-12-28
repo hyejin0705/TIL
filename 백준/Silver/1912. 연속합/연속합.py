b318403ee940c2303f11ab7166a6e178d6282541
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2022-12-29 00:01:16 +0900

    [Silver II] Title: 연속합, Time: 88 ms, Memory: 38432 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,8 +1,13 @@
-n = int(input())
-array = list(map(int, input().split()))
+import sys
 
-d = [0] * n
-d[0] = array[0]
-for i in range(1, n):
-    d[i] = max(array[i], d[i-1]+array[i])
+N = int(input())
+
+lst = list(map(int, sys.stdin.readline().split()))
+
+d = [0] * N
+
+d[0] = lst[0]
+for i in range(1, N):
+    d[i] = max(lst[i], d[i-1] + lst[i])
+
 print(max(d))
