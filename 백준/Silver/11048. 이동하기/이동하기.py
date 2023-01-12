eb7823a93d8d17c3144b882ca7ead2ce0f0e6845
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-01-12 23:58:33 +0900

    [Silver II] Title: 이동하기, Time: 744 ms, Memory: 38408 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split())
-dp = [[0] * (M + 1)] * (N + 1)
-candy = []
 
-for i in range(N):
-    candy.append(list(map(int, input().split())))
+arr = [[0] * (M + 1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + candy[i-1][j-1]
+check = [[0] * (M + 1)] * (N + 1)
 
-print(dp[N][M])
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        MAX = max(check[i-1][j], check[i][j-1])
+        check[i][j] = arr[i][j] + MAX
+
+print(check[N][M])
