1cac4e1125c7ffba8dd57a05b3a496cfaffe7a62
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-01-18 00:03:10 +0900

    [Silver V] Title: UCPC는 무엇의 약자일까?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
+++ 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
@@ -1,12 +1,13 @@
 check = input()
 
 ans = ['U', 'C', 'P', 'C']
+
 for a in ans:
-    if a in check:
-        check = check[check.index(a) + 1:]
+    if a in check:   # 문자열에 'UCPC' 해당 문자가 존재하면,
+        check = check[check.index(a) + 1:]   # 문자열 재정의
     else:
-        print('I hate UCPC')
+        print('I hate UCPC')   # 없다면, 실패
         break
 
-else:
+else:     # 중간에 종료되지 않고, 끝까지 돌았다면, 성공
     print('I love UCPC')
