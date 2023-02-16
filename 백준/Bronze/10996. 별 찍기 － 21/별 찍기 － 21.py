ccbac0cfbf67e67eecee321a6deb7e8ef10c23c5
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-02-17 05:44:22 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.py
@@ -0,0 +1,8 @@
+inputNum = int(input())
+odd = inputNum - inputNum // 2 # 홀수번째 
+even = inputNum // 2 # 짝수번째
+
+# Main
+for i in range(inputNum):
+        print("* " * odd)
+        print(" *" * even)
