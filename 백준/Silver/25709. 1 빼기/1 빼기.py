0c76648f13347f0f7986dff6048f16f53a384759
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-02-24 05:20:34 +0900

    [Silver III] Title: 1 빼기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25709. 1 빼기/1 빼기.py
+++ 백준/Silver/25709. 1 빼기/1 빼기.py
@@ -1,18 +1,18 @@
 N = input()
 ans = 0
 while N:
-    if int(N) == 1:
+    if int(N) == 1:    # 1이면, 끝
         ans += 1
         break
 
-    if N.count('1'):
-        idx = N.index('1')
-        N = N[:idx] + N[idx+1:]
-        if not int(N):
+    if N.count('1'):           # 1의 존재 여부 파악
+        idx = N.index('1')       # 1이 존재하면, 위치 파악
+        N = N[:idx] + N[idx+1:]   # 1 빼기
+        if not int(N):            # 1를 뺐는데, 0이라면, 종료
             ans += 1
             break
     else:
-        N = str(int(N) - 1)
-    ans += 1
+        N = str(int(N) - 1)      # 빼고 다시 문자열로 변환
+    ans += 1    # 전체적으로 한바퀴 돌았다면, 1회 실행
 
 print(ans)
