7f083ef4b72944714f9ac27156588ee7f8a09b92
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-03-02 02:08:12 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 836 ms, Memory: 73244 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
@@ -1,25 +1,37 @@
+# 분할하지 않으면, 수열의 길이가 40이기 때문에,
+# 모든 경우의 수가 2 ** 40으로 시간초과 걸림.
+
+# 해결방안 >>> 왼쪽, 오른쪽 나누어서 탐색
+
+# dfs 함수를 활용하는 건 똑같음.
 def dfs(idx, E, add, side):
-    global S, N, res
-    if idx == E:
-        if side == 'L':
-            if not add in leftNums:
-                leftNums[add] = 1
-            else:
-                leftNums[add] += 1
-        elif S - add in leftNums:
-            res += leftNums[S - add]
+    global S, N, ans
+    if idx == E:       # 종료조건
+        if side == 'L':              # 왼쪽 수열일때,
+            if add not in leftNums:  # 없다면,
+                leftNums[add] = 1    # 생성
+            else:                    # 있다면,
+                leftNums[add] += 1   # 1 증가
+
+        # 목표 - 왼쪽 합 = 오른쪽 합
+        elif S - add in leftNums:     # 오른쪽 수열일 때, 확인하기
+            ans += leftNums[S - add]
+
         return
 
     dfs(idx + 1, E, add + nums[idx], side)
     dfs(idx + 1, E, add, side)
 
 
+# N: 수열의 개수, S: 목표 합
 N, S = map(int, input().split())
 
+# 수열 리스트
 nums = list(map(int, input().split()))
 
-res = 0
+ans = 0    # 정답
 
+# 왼쪽 수열의 합
 leftNums = {}
 
 # 왼쪽 수열
@@ -28,7 +40,8 @@ dfs(0, N // 2, 0, 'L')
 # 오른쪽 수열
 dfs(N // 2, N, 0, 'R')
 
+# 공집합 + 공집합인 경우 빼주기
 if S == 0:
-    res -= 1
+    ans -= 1
 
-print(res)
+print(ans)
