cd752734a9ceafbe68ad33e2f5dee095c6303158
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-03-12 23:38:26 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 6040 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -1,10 +1,9 @@
 n = int(input())
-eggs = []
+
 answer = 0
 S, W = 0, 1
 
-for _ in range(n):
-    eggs.append(list(map(int, input().split())))
+eggs = [list(map(int, input().split())) for _ in range(n)]
 
 def crash(nowIndex):
     global answer
@@ -21,7 +20,7 @@ def crash(nowIndex):
     if eggs[nowIndex][S] <= 0:
         crash(nowIndex + 1)
         return
-    
+
     # 자기말고 다 깨져있는 상황인 경우
     isAllBroken = True
     for targetIndex in range(n):
@@ -29,21 +28,26 @@ def crash(nowIndex):
         if eggs[targetIndex][S] > 0:
             isAllBroken = False
             break
+            
     if isAllBroken:
         answer = max(answer, n - 1)
         return
 
     # 때려보기
     for targetIndex in range(n):
-        if targetIndex == nowIndex: continue
-        if eggs[targetIndex][S] <= 0: continue
+        if targetIndex == nowIndex: 
+            continue
+        if eggs[targetIndex][S] <= 0: 
+            continue
+        
         # 때리기
         eggs[nowIndex][S] -= eggs[targetIndex][W]
         eggs[targetIndex][S] -= eggs[nowIndex][W]
         crash(nowIndex + 1)
+        
         # 복구
         eggs[nowIndex][S] += eggs[targetIndex][W]
         eggs[targetIndex][S] += eggs[nowIndex][W]
-        
+
 crash(0)
 print(answer)
