c564e725747c15e2f7486e8ccbdf5f494833deec
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-03-28 05:29:03 +0900

    [Gold V] Title: 동전 2, Time: 440 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,14 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+coins = sorted([int(sys.stdin.readline()) for _ in range(N)])
+
+arr = [10001] * (K+1)
+arr[0] = 0
+
+for i in range(N):
+    for j in range(coins[i], K+1):
+        arr[j] = min(arr[j], arr[j-coins[i]] + 1)
+
+arr[-1] = arr[-1] if arr[-1] != 10001 else -1
+print(arr[-1])
