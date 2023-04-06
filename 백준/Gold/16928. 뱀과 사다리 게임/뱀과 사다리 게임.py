f9a633daad677a82f914647c5b912c83f7dd75ae
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-04-06 20:37:21 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+board = []
+visited = [False] * 110
+for i in range(0,110) :
+    board.append([i,i])
+
+n, m = map(int, sys.stdin.readline().split())
+for _ in range(n+m) :
+    x, y = map(int, sys.stdin.readline().split())
+    board[x][1] = y
+
+li = deque([[1,0]])
+
+def roll(cur) :
+    for i in range(1,7) :
+        temp = board[cur[0]+i][1]
+        count = cur[1] + 1
+        if visited[temp] == False :
+            li.append([temp,count])
+            visited[temp] = True
+
+while True :
+    cur = li.popleft()
+    if cur[0] == 100 :
+        print(cur[1])
+        break
+    else :
+        roll(cur)
