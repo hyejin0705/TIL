2dc88102024a2540e6dbccbb9207f60e573f849f
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-06-02 23:49:13 +0900

    [Gold V] Title: 줄어드는 수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -0,0 +1,25 @@
+N = int(input())
+state = False
+elements = [str(n) for n in range(10)]
+nums = [[] for _ in range(10)]
+if N < 1024:
+    for i in range(1, 1 << 10):
+        num = ''
+        cnt = 0
+        for j in range(10):
+            if (i & 1 << j):
+                num = elements[j] + num
+                cnt += 1
+        nums[cnt - 1].append(num)
+    cnt = 0
+    for e in nums:
+        if state:
+            break
+        for n in e:
+            cnt += 1
+            if cnt == N:
+                state = True
+                print(n)
+                break
+else:
+    print(-1)
