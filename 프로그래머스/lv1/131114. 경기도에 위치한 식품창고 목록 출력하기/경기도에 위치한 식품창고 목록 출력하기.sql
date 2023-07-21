75a6c947e1be782ff6f3e224ae5dea72aa7f238a
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-07-22 00:16:17 +0900

    [level 1] Title: 경기도에 위치한 식품창고 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,9 +1,19 @@
--- 코드를 입력하세요
+-- 풀이 1
+-- SELECT warehouse_id, warehouse_name, address, 
+--     CASE 
+--         WHEN freezer_yn IS NULL THEN 'N' 
+--         ELSE freezer_yn
+--     END AS freezer_yn
+-- FROM food_warehouse
+-- WHERE address LIKE '경기도%'
+-- ORDER BY warehouse_id;
+
+
+-- 풀이 2
 SELECT warehouse_id, warehouse_name, address, 
-    CASE 
-        WHEN freezer_yn IS NULL THEN 'N' 
-        ELSE freezer_yn
-    END AS freezer_yn
+    NVL(freezer_yn, 'N') as freezer_yn
+    -- NVL 함수는 값이 NULL인 경우 지정값을 출력하고, NULL이 아니면 원래 값을 그대로 출력한다.
+    -- 함수  :  NVL("값", "지정값") 
 FROM food_warehouse
 WHERE address LIKE '경기도%'
 ORDER BY warehouse_id;
