290aa58cb6d23c8d4ee6a2179e976b203ec91f8d
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-08-12 23:22:52 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,7 @@
 -- TO_CHAR(datetime, 'HH') : 01 ~ 12
 -- TO_CHAR(datetime, 'HH24') : 01 ~ 24
 
+-- TO_NUMBER로 형변환하지 않으면, 오답처리 됨.
 SELECT TO_NUMBER(TO_CHAR(datetime, 'HH24')) AS hour, 
     count(animal_id) AS count
 FROM animal_outs
