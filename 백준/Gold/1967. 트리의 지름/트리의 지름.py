30ac13c9c5283be702faa34812531657a0461916
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-08-16 23:20:19 +0900

    [Gold IV] Title: 트리의 지름, Time: 88 ms, Memory: 36916 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+def dfs(cur, result):
+    for i in range(len(tree[cur])):
+        end, dis = tree[cur][i]
+        
+        if result[end] == 0:
+            result[end] = result[cur] + dis
+            dfs(end, result)
+
+n = int(sys.stdin.readline())
+
+result1=[0 for _ in range(n+1)]
+result2=[0 for _ in range(n+1)]
+tree=[[] for _ in range(n+1)]
+
+for _ in range(n - 1):
+    a, b, c = map(int, sys.stdin.readline().split())
+    tree[a].append((b,c))
+    tree[b].append((a,c))
+
+dfs(1, result1)
+result1[1] = 0
+maxi = 0
+index = 0
+
+for i in range(len(result1)):
+    if maxi < result1[i]:
+        maxi = result1[i]
+        index = i
+
+dfs(index, result2)
+result2[index] = 0
+
+print(max(result2))
