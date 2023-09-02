09671c7752053b0cf82975239b6af55a938cbbba
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-09-02 23:38:27 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,11 +1,22 @@
--- 코드를 입력하세요
-SELECT '/home/grep/src/' || board_id || '/' || file_id || file_name || file_ext AS file_path
-FROM used_goods_file
-WHERE board_id IN ( SELECT board_id
-                    FROM ( SELECT board_id
-                           FROM used_goods_board
-                           ORDER BY views DESC
-                         )
-                    WHERE ROWNUM = 1
-                  )
+-- 1.
+-- SELECT '/home/grep/src/' || board_id || '/' || file_id || file_name || file_ext AS file_path
+-- FROM used_goods_file
+-- WHERE board_id IN ( SELECT board_id
+--                     FROM ( SELECT board_id
+--                            FROM used_goods_board
+--                            ORDER BY views DESC
+--                          )
+--                     WHERE ROWNUM = 1
+--                   )
+-- ORDER BY file_id DESC;
+
+
+-- 2. Join 활용하기
+SELECT '/home/grep/src/' || f.board_id || '/' || file_id || file_name || file_ext AS file_path
+FROM used_goods_file f
+LEFT JOIN used_goods_board b
+ON f.board_id = b.board_id
+WHERE views = ( SELECT max(views)
+                FROM used_goods_board
+              )
 ORDER BY file_id DESC;
