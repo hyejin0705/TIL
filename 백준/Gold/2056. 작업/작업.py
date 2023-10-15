1c3bea83d8c84feee475b1967bf2449abc2a8a9d
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-10-15 23:42:12 +0900

    [Gold IV] Title: 작업, Time: 348 ms, Memory: 52728 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+time = [0]*(n+1)                # 해당 노드에서 걸리는 시간
+pre = [[] for _ in range(n+1)]  # 선행해야하는 노드 번호들
+
+for i in range(1, n+1):
+    temp = list(map(int, sys.stdin.readline().split()))
+    time[i] = temp[0]
+    if temp[1] != 0:
+        pre[i] = temp[2:]
+
+for i in range(1, n+1):
+    temp = 0   
+    for j in pre[i]:    # 선행작업이 존재하는 경우
+        temp = max(temp, time[j])
+    time[i] += temp
+print(max(time))
