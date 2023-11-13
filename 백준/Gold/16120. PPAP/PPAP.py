30948510969baaff1f689f06c3d0173997f323e6
hyejin0705 <109321988+hyejin0705@users.noreply.github.com>
2023-11-14 01:05:37 +0900

    [Gold IV] Title: PPAP, Time: 424 ms, Memory: 38168 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -0,0 +1,14 @@
+s = input()
+stack = []
+ppap = ["P", "P", "A", "P"]
+
+for i in range(len(s)):
+    stack.append(s[i])
+    if stack[-4:] == ppap:
+        for _ in range(4):
+            stack.pop()
+        stack.append("P")
+if stack == ppap or stack == ["P"]:
+    print("PPAP")
+else:
+    print("NP")
